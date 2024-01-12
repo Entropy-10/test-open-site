1400b975e5ae3dbcf918503c4a2cf81c56949fd8
Entropy <45132530+Entropy-10@users.noreply.github.com>
2024-01-11 23:32:26 -0800

    feat: added team management

---
+++ app/[locale]/team/_components/players.tsx
@@ -0,0 +1,33 @@
+import { cookies } from 'next/headers'
+import { createClient } from '@supabase/server'
+
+import Player from './player'
+
+interface PlayersProps {
+  teamId: string
+}
+
+export default async function Players({ teamId }: PlayersProps) {
+  const supabase = createClient(cookies())
+  const { data: players } = await supabase
+    .from('players')
+    .select('*, users(*)')
+    .eq('team_id', teamId)
+
+  if (!players?.[0]) {
+    return (
+      <div className='flex h-[311px] items-center justify-center'>
+        Sorry, we failed to get the players on your team.
+      </div>
+    )
+  }
+
+  //todo: make this a carousel to match design and prevent squishing of player cards
+  return (
+    <div className='padding my-5 flex w-full gap-5'>
+      {players.map(player => (
+        <Player key={player.user_id} player={player} />
+      ))}
+    </div>
+  )
+}
---
+++ components/section-loader.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@utils/client'
+import { Loader2 } from 'lucide-react'
+
+interface SectionLoaderProps {
+  className?: string
+}
+
+export default function SectionLoader({ className }: SectionLoaderProps) {
+  return (
+    <div
+      className={cn('flex h-48 items-center justify-center gap-2', className)}
+    >
+      <Loader2 size={25} strokeWidth={3} className='animate-spin' />
+    </div>
+  )
+}
